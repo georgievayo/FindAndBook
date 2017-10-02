99a6b00b472891b2f27cf0e631e1b94664b004b8
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-02 22:38:43 +0300

    Add booking form

---
+++ FindAndBook/FindAndBook.Web/Controllers/BookingsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Mvc;
+using FindAndBook.Authentication.Contracts;
+using FindAndBook.Services.Contracts;
+
+namespace FindAndBook.Web.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private readonly IAuthenticationProvider authProvider;
+        private readonly IPlaceService placeService;
+        private readonly IBookingService bookingService;
+
+        public BookingsController(IAuthenticationProvider authProvider, 
+            IPlaceService placeService, IBookingService bookingService)
+        {
+            if (authProvider == null)
+            {
+                throw new ArgumentNullException(nameof(authProvider));
+            }
+
+            if (placeService == null)
+            {
+                throw new ArgumentNullException(nameof(placeService));
+            }
+
+            if (bookingService == null)
+            {
+                throw new ArgumentNullException(nameof(bookingService));
+            }
+
+            this.authProvider = authProvider;
+            this.placeService = placeService;
+            this.bookingService = bookingService;
+        }
+
+        public ActionResult GetBookingForm()
+        {
+            if (authProvider.IsAuthenticated)
+            {
+                return PartialView("_BookingDateTimeForm");
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+
+        //public ActionResult PostBookingForm()
+        //{
+            
+        //}
+    }
+}
---
+++ FindAndBook/FindAndBook.Web/Models/Places/BookingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FindAndBook.Web.Models.Places
+{
+    public class BookingViewModel
+    {
+        public DateTime DateTime { get; set; }
+    }
+}
