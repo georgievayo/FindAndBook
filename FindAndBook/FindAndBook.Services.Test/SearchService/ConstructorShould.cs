a3682b0915bb24a3df34be79169de773a2affc62
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-11 12:59:43 +0300

    Test search service

---
+++ FindAndBook/FindAndBook.Services.Test/SearchService/ConstructorShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindAndBook.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace FindAndBook.Services.Test.SearchService
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ConstructorShould_SetPassedDataCorrectlly_WhenDataIsNotNull()
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+
+            Assert.IsNotNull(service);
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowException_WhenDataIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Services.SearchService(null));
+        }
+    }
+}
---
+++ FindAndBook/FindAndBook.Services.Test/SearchService/FindByShould.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections.Generic;
+using FindAndBook.Models;
+using FindAndBook.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System.Linq;
+
+namespace FindAndBook.Services.Test.SearchService
+{
+    [TestFixture]
+    public class FindByShould
+    {
+        [TestCase("Restaurant", "Name", "pesho")]
+        [TestCase("Club", "Name", "pesho")]
+        [TestCase("Cafe", "Name", "pesho")]
+        public void FindByShould_CallPlaceServiceMethodFindInName_WhenSearchByIsName(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+
+            service.FindBy(category, searchBy, pattern);
+
+            placeServiceMock.Verify(s => s.FindInName(category, pattern));
+        }
+
+        [TestCase("Restaurant", "Name", "pesho")]
+        [TestCase("Club", "Name", "pesho")]
+        [TestCase("Cafe", "Name", "pesho")]
+        public void FindByShould_ReturnCorrectResult_WhenSearchByIsName(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+            var place = new Place() {Name = "Pesho's place"};
+            var list = new List<Place>() {place};
+            placeServiceMock.Setup(s => s.FindInName(category, pattern)).Returns(list.AsQueryable());
+
+            var result = service.FindBy(category, searchBy, pattern);
+
+            Assert.Contains(place, result.ToList());
+        }
+
+        [TestCase("Restaurant", "Description", "pesho")]
+        [TestCase("Club", "Description", "pesho")]
+        [TestCase("Cafe", "Description", "pesho")]
+        public void FindByShould_CallPlaceServiceMethodFindInDescription_WhenSearchByIsDescription(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+
+            service.FindBy(category, searchBy, pattern);
+
+            placeServiceMock.Verify(s => s.FindInDescription(category, pattern));
+        }
+
+        [TestCase("Restaurant", "Description", "pesho")]
+        [TestCase("Club", "Description", "pesho")]
+        [TestCase("Cafe", "Description", "pesho")]
+        public void FindByShould_ReturnCorrectResult_WhenSearchByIsDescription(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+            var place = new Place() { Details = "Pesho's place" };
+            var list = new List<Place>() { place };
+            placeServiceMock.Setup(s => s.FindInDescription(category, pattern)).Returns(list.AsQueryable());
+
+            var result = service.FindBy(category, searchBy, pattern);
+
+            Assert.Contains(place, result.ToList());
+        }
+
+        [TestCase("Restaurant", "Address", "pesho")]
+        [TestCase("Club", "Address", "pesho")]
+        [TestCase("Cafe", "Address", "pesho")]
+        public void FindByShould_CallPlaceServiceMethodFindInAddress_WhenSearchByIsAddress(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+
+            service.FindBy(category, searchBy, pattern);
+
+            placeServiceMock.Verify(s => s.FindInAddress(category, pattern));
+        }
+
+        [TestCase("Restaurant", "Address", "pesho")]
+        [TestCase("Club", "Address", "pesho")]
+        [TestCase("Cafe", "Address", "pesho")]
+        public void FindByShould_ReturnCorrectResult_WhenSearchByIsAddress(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+            var address = new Address() {Street = "Pesho's street"};
+            var place = new Place() { Address = address };
+            var list = new List<Place>() { place };
+            placeServiceMock.Setup(s => s.FindInAddress(category, pattern)).Returns(list.AsQueryable());
+
+            var result = service.FindBy(category, searchBy, pattern);
+
+            Assert.Contains(place, result.ToList());
+        }
+
+        [TestCase("Restaurant", "Average bill", "15")]
+        [TestCase("Club", "Average bill", "15")]
+        [TestCase("Cafe", "Average bill", "15")]
+        public void FindByShould_CallPlaceServiceMethodFindInBill_WhenSearchByIsAverageBill(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+
+            service.FindBy(category, searchBy, pattern);
+
+            placeServiceMock.Verify(s => s.FindInBill(category, pattern));
+        }
+
+        [TestCase("Restaurant", "Average bill", "15")]
+        [TestCase("Club", "Average bill", "15")]
+        [TestCase("Cafe", "Average bill", "15")]
+        public void FindByShould_ReturnCorrectResult_WhenSearchByIsAverageBill(string category, string searchBy, string pattern)
+        {
+            var placeServiceMock = new Mock<IPlaceService>();
+
+            var service = new Services.SearchService(placeServiceMock.Object);
+            var place = new Place() { AverageBill = 15 };
+            var list = new List<Place>() { place };
+            placeServiceMock.Setup(s => s.FindInBill(category, pattern)).Returns(list.AsQueryable());
+
+            var result = service.FindBy(category, searchBy, pattern);
+
+            Assert.Contains(place, result.ToList());
+        }
+    }
+}
