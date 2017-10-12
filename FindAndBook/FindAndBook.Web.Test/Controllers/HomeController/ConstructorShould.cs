5075176746f24624d6cc36fef5667cd6ad46bd6f
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-12 18:24:14 +0300

    Test part of HomeController

---
+++ FindAndBook/FindAndBook.Web.Test/Controllers/HomeController/ConstructorShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FindAndBook.Web.Factories;
+using Moq;
+using NUnit.Framework;
+
+namespace FindAndBook.Web.Test.Controllers.HomeController
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ConstructorShould_SetPassedDataCorrectly_WhenDataIsNotNull()
+        {
+            var factoryMock = new Mock<IViewModelFactory>();
+
+            var controller = new Web.Controllers.HomeController(factoryMock.Object);
+
+            Assert.IsNotNull(controller);
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowException_WhenDataIsNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Web.Controllers.HomeController(null));
+        }
+    }
+}
