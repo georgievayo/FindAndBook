c53a1932693e611459754a5a2d31a3843793be07
Yoana Georgieva <yo.ana.georgieva@abv.bg>
2017-10-14 20:37:28 +0300

    Test QuestionService

---
+++ FindAndBook/FindAndBook.Services.Test/QuestionService/ConstructorShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using FindAndBook.Data.Contracts;
+using FindAndBook.Factories;
+using FindAndBook.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace FindAndBook.Services.Test.QuestionService
+{
+    [TestFixture]
+    public class ConstructorShould
+    {
+        [Test]
+        public void ConstructorShould_SetPassedData_WhenDataIsNotNull()
+        {
+            var repositoryMock = new Mock<IRepository<Question>>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var factoryMock = new Mock<IQuestionFactory>();
+
+            var service = new Services.QuestionService(repositoryMock.Object, unitOfWorkMock.Object,
+                factoryMock.Object);
+
+            Assert.IsNotNull(service);
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowException_WhenRepositoryIsNull()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var factoryMock = new Mock<IQuestionFactory>();
+
+            Assert.Throws<ArgumentNullException>(() => new Services.QuestionService(null, unitOfWorkMock.Object,
+                factoryMock.Object));
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowException_WhenUnitOfWorkIsNull()
+        {
+            var repositoryMock = new Mock<IRepository<Question>>();
+            var factoryMock = new Mock<IQuestionFactory>();
+
+            Assert.Throws<ArgumentNullException>(() => new Services.QuestionService(repositoryMock.Object, null,
+                factoryMock.Object));
+        }
+
+        [Test]
+        public void ConstructorShould_ThrowException_WhenFactoryIsNull()
+        {
+            var repositoryMock = new Mock<IRepository<Question>>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+
+            Assert.Throws<ArgumentNullException>(() => new Services.QuestionService(repositoryMock.Object, unitOfWorkMock.Object,
+                null));
+        }
+    }
+}
---
+++ FindAndBook/FindAndBook.Services.Test/QuestionService/GetAllShould.cs
@@ -0,0 +1,27 @@
+﻿using FindAndBook.Data.Contracts;
+using FindAndBook.Factories;
+using FindAndBook.Models;
+using Moq;
+using NUnit.Framework;
+
+namespace FindAndBook.Services.Test.QuestionService
+{
+    [TestFixture]
+    public class GetAllShould
+    {
+        [Test]
+        public void GetAllShould_CallRepositoryPropertyAll()
+        {
+            var repositoryMock = new Mock<IRepository<Question>>();
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var factoryMock = new Mock<IQuestionFactory>();
+
+            var service = new Services.QuestionService(repositoryMock.Object, unitOfWorkMock.Object,
+                factoryMock.Object);
+
+            service.GetAll();
+
+            repositoryMock.Verify(r => r.All, Times.Once);
+        }
+    }
+}
